1ad38a3a7e567c219e2d508b760593ef094ebc8d
Jakub Biesiada <kbba@op.pl>
2019-10-12 21:40:35 +0200

    Fix error when betas dir is empty or not exists (#17)

--- src/check-betas.js
+++ src/check-betas.js
@@ -2,14 +2,22 @@ const fs = require('fs');
 
 const updateBetas = require('./update-betas');
 
-const checkBetas = releases => {
-  const files = fs
-    .readdirSync('./betas')
-    .sort((prev, next) => (prev > next ? -1 : 1));
+const { betasDir } = require('../package.json');
 
-  fs.readFile(`./betas/${files[0]}`, 'utf8', (err, oldReleases) => {
-    if (oldReleases !== releases) updateBetas(oldReleases, releases);
-  });
+const checkBetas = releases => {
+  if (fs.existsSync(betasDir)) {
+    const files = fs
+      .readdirSync(betasDir)
+      .sort((prev, next) => (prev > next ? -1 : 1));
+
+    fs.readFile(`${betasDir}/${files[0]}`, 'utf8', (err, oldReleases) => {
+      if (oldReleases !== releases) updateBetas(releases, oldReleases);
+    });
+  } else {
+    fs.mkdirSync(betasDir);
+
+    updateBetas(releases);
+  }
 };
 
 module.exports = checkBetas;
