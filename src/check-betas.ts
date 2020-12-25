138bee95da2408f3636fc00d6e09ca6f741f6919
Jakub Biesiada <biesiadajakub@icloud.com>
2020-12-25 23:59:19 +0100

    refactor: split code into smaller functions

--- src/check-betas.ts
+++ src/check-betas.ts
@@ -5,16 +5,24 @@ import updateBetas from './update-betas';
 
 const { betasDir } = require('../package.json');
 
-const checkBetas = (releases: string) => {
-  if (fs.existsSync(betasDir)) {
-    const files = fs
-      .readdirSync(betasDir)
-      .sort((prev, next) => (prev > next ? -1 : 1));
+const getPrevBetaStamps = () => fs.readdirSync(betasDir);
 
-    fs.readFile(`${betasDir}/${files[0]}`, 'utf8', (err, oldReleases) => {
+const sortItems = (items: string[]) => {
+  return items.sort((prev, next) => (prev > next ? -1 : 1));
+};
+
+const checkBetas = (releases: string) => {
+  const isPreviousBetasExists = fs.existsSync(betasDir);
+
+  if (isPreviousBetasExists) {
+    const betaStampFiles = sortItems(getPrevBetaStamps());
+
+    const latestBetaStampUrl = `${betasDir}/${betaStampFiles[0]}`;
+
+    fs.readFile(latestBetaStampUrl, 'utf8', (err, oldReleases) => {
       if (err) throw err;
 
-      if (oldReleases !== releases) {
+      if (releases !== oldReleases) {
         updateBetas(releases, oldReleases);
       } else {
         console.log(chalk.green('No new beta releases found 🤗'));
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import puppeteer from 'puppeteer';
 import striptags from 'striptags';
+import { config } from 'dotenv';
 
 import checkBetas from './check-betas';
 
+config();
+
 const FEED_URL = 'https://developer.apple.com/news/releases';
+
+const getReleaseTitles = () => {
+  const elements = Array.from(
+    document.querySelectorAll('.article-content-container h2')
+  );
+
+  return elements.map((element) => element.innerHTML).join('\n');
+};
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -14,13 +25,7 @@ const FEED_URL = 'https://developer.apple.com/news/releases';
 
   await page.goto(FEED_URL);
 
-  const releases = await page.evaluate(() => {
-    const elements = Array.from(
-      document.querySelectorAll('.article-content-container h2')
-    );
-
-    return elements.map((element) => element.innerHTML).join('\n');
-  });
+  const releases = await page.evaluate(getReleaseTitles);
 
   checkBetas(striptags(releases));
 
